2af654c477e520d312ac8b3393f2a83f98924900
ImNia <agnia-kimi1996@mail.ru>
2023-02-20 13:21:20 +0700

    added night theme and font

--- app/src/main/java/com/delirium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/delirium/playlistmaker/SettingsActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
