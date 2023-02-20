312326130ea2f5f4fc9a15a32b7dcba2d403e008
ImNia <agnia-kimi1996@mail.ru>
2023-02-20 18:01:28 +0700

    Handle button in setting screen

--- app/src/main/java/com/delirium/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/delirium/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,20 @@
 package com.delirium.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity: AppCompatActivity() {
 
+    private lateinit var sharingApp: TextView
+    private lateinit var messageSupport: TextView
+    private lateinit var termsUser: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -15,5 +24,36 @@ class SettingsActivity: AppCompatActivity() {
         supportActionBar?.title = getString(R.string.setting)
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 //        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        sharingApp = findViewById(R.id.sharing_app)
+        messageSupport = findViewById(R.id.message_support)
+        termsUser = findViewById(R.id.terms_user)
+
+        sharingApp.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, LINK_TO_SITE)
+            shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+        }
+        messageSupport.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(DEVELOPER_MAIL))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, THEME_TO_SUPPORT)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, MESSAGE_TO_SUPPORT)
+            startActivity(supportIntent)
+        }
+        termsUser.setOnClickListener {
+            val address = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val termIntent = Intent(Intent.ACTION_VIEW, address)
+            startActivity(termIntent)
+        }
+    }
+
+    companion object {
+        const val THEME_TO_SUPPORT = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        const val MESSAGE_TO_SUPPORT = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        const val LINK_TO_SITE = "https://practicum.yandex.ru/profile/android-developer/"
+        const val DEVELOPER_MAIL = "delirium.ag@yandex.ru"
     }
 }
