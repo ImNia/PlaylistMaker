e22a84a0a91433aec21359cdcb2e2e2e1886f768
ImNia <agnia-kimi1996@mail.ru>
2023-07-17 10:29:15 +0700

    Added bottomNavigationView

--- app/src/main/java/com/delirium/playlistmaker/main/ui/activity/MainActivity.kt
+++ app/src/main/java/com/delirium/playlistmaker/main/ui/activity/MainActivity.kt
@@ -2,11 +2,21 @@ package com.delirium.playlistmaker.main.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.delirium.playlistmaker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
